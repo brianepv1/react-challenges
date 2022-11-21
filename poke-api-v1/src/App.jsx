72ce911ba78cb41180c8685880899a7f8d97d3c1
Brian Palomar <brian.epv1@gmail.com>
2022-11-21 15:47:17 -0600

    Base code folder and structure

---
+++ poke-api-v1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+    </div>
+  )
+}
+
+export default App
